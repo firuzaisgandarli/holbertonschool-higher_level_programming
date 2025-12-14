e6439114ace74b84d5df02061f3bfa4aa4e5b2a9
FiruzÉzaIsgandarli <firuze.isgenderli41@gmail.com>
2025-12-14 19:37:32 +0000

    Add Student class with to_json filter method

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with filter for JSON serialization"""
+
+
+class Student:
+    """Class that defines a student by first_name, last_name, and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of a Student instance.
+        If attrs is a list of strings, only those attributes are returned.
+        Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
