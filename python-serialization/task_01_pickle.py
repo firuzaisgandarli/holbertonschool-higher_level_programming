2cc700f66ec399c2234431ceeca66d6dce3b2e5d
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2025-12-19 19:36:30 +0000

    Add task_01_pickle.py for custom class pickling

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name="", age=0, is_student=False):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Print object attributes in required format"""
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        """Serialize current instance to a file"""
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except Exception:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize object from a file"""
+        try:
+            with open(filename, 'rb') as f:
+                obj = pickle.load(f)
+                if isinstance(obj, cls):
+                    return obj
+                return None
+        except Exception:
+            return None
