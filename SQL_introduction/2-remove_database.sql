e897f890e32b9355513834335556ffe0dd71c788
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2025-12-19 19:57:36 +0000

    Add 2-remove_database.sql to remove hbtn_0c_0 database

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- 2. Remove database
+-- This script deletes the database hbtn_0c_0 from the MySQL server
+-- If the database does not exist, the script will not fail
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
