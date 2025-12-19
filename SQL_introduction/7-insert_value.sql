8f65a2ca2bc676223e4c0c1dce2c1582930abf3f
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2025-12-19 20:12:48 +0000

    Add 7-insert_value.sql to insert new row into first_table

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- 7. First add
+-- This script inserts a new row into the table first_table
+-- from the database hbtn_0c_0 in the MySQL server
+-- New row: id = 89, name = 'Best School'
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
