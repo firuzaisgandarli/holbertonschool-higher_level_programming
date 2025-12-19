0dd5e44887e778219ddd57f5ebfe03ea769b56bd
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2025-12-19 20:03:39 +0000

    Add 6-list_values.sql to list all rows from first_table

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,6 @@
+-- 6. List all in table
+-- This script lists all rows of the table first_table
+-- from the database hbtn_0c_0 in the MySQL server
+-- All fields should be printed
+
+SELECT * FROM first_table;
