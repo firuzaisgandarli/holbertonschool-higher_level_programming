3f5ae522aaa9b703e523e83a245bc8d4a7a7d344
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2025-12-19 21:03:05 +0000

    Add 14-average.sql to compute average score from second_table

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- 14. Average
+-- This script computes the score average of all records
+-- in the table second_table of the database hbtn_0c_0
+-- The result column name should be 'average'
+
+SELECT AVG(score) AS average FROM second_table;
