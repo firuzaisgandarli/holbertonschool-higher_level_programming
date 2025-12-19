8406dc77fd75a54c31f12cf1f6028cf419cbd436
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2025-12-19 19:56:15 +0000

    Add 1-create_database_if_missing.sql to create hbtn_0c_0 database

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- 1. Create database if missing
+-- This script creates the database hbtn_0c_0 in MySQL server
+-- If the database already exists, the script will not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
