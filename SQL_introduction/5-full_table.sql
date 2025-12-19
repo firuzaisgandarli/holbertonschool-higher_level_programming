a989b7433353da4652d4aa2c472aceb516a2ee20
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2025-12-19 20:02:16 +0000

    Add 5-full_table.sql to show full description of first_table

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+-- 5. Full description
+-- This script prints the full description of the table first_table
+-- from the database hbtn_0c_0 in the MySQL server
+-- DESCRIBE and EXPLAIN statements are not allowed
+
+SHOW CREATE TABLE first_table;
