42dd4bf0bade54d2a2fcd17c8b463df7f96c192a
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2025-12-20 21:39:36 +0000

    Add 11-genre_id_all_shows.sql to list all shows with genre_id or NULL

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,11 @@
+-- 11. Genre ID for all shows
+-- This script lists all shows contained in the database hbtn_0d_tvshows
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesn’t have a genre, display NULL
+-- Only one SELECT statement is allowed
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
