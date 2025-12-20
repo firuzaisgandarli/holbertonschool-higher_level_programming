3e9bbbc7464fd0133251d4e93cdfa47ed20deff8
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2025-12-20 21:24:59 +0000

    Add 2-create_read_user.sql to create hbtn_0d_2 and user_0d_2 with SELECT privilege

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- 2. Read user
+-- This script creates the database hbtn_0d_2 and the user user_0d_2
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- The password for user_0d_2 is set to 'user_0d_2_pwd'
+-- If the database already exists, the script will not fail
+-- If the user already exists, the script will not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
