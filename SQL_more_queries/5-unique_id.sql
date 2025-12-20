700b037bcc4c5bfdc8835cbeb0a6f80268342880
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2025-12-20 21:32:05 +0000

    Add 5-unique_id.sql to create unique_id table with unique id field

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,12 @@
+-- 5. Unique ID
+-- This script creates the table unique_id on the MySQL server
+-- Table description:
+--   id INT with the default value 1 and must be unique
+--   name VARCHAR(256)
+-- The database name will be passed as an argument of the mysql command
+-- If the table unique_id already exists, the script will not fail
+
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
